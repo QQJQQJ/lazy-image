97d26cb5da3b28481516cce9964c7f1444ade84a
turbo <1004891514@qq.com>
2019-02-18 09:08:21 +0800

    lazy-image

---
+++ unpackage/dist/dev/app-plus/pages/index/index.js
@@ -0,0 +1,13 @@
+
+      !(function(){
+        var uniAppViewReadyCallback = function(){
+          setCssToHead([".",[1],"content { text-align: center; height: ",[0,400],"; }\n.",[1],"title-box{ height: ",[0,200],"; width: 100%; display: -webkit-box; display: -webkit-flex; display: -ms-flexbox; display: flex; -webkit-box-orient: vertical; -webkit-box-direction: normal; -webkit-flex-direction: column; -ms-flex-direction: column; flex-direction: column; -webkit-box-align: center; -webkit-align-items: center; -ms-flex-align: center; align-items: center; -webkit-justify-content: space-around; -ms-flex-pack: distribute; justify-content: space-around; }\n.",[1],"title { font-size: ",[0,36],"; color: #8f8f94; }\n.",[1],"navigator{ width: 100%; height: 100%; }\n",],undefined,{path:"./pages/index/index.wxss"})();
+document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/index/index.wxml') } }));
+        }
+        if(window.__uniAppViewReady__){
+          uniAppViewReadyCallback()
+        }else{
+          document.addEventListener('uniAppViewReady',uniAppViewReadyCallback)
+        }
+      })();
+      
---
+++ unpackage/dist/dev/app-plus/pages/lazy-load/lazy-load.js
@@ -0,0 +1,13 @@
+
+      !(function(){
+        var uniAppViewReadyCallback = function(){
+          setCssToHead(["@charset \x22UTF-8\x22;\n.",[1],"lazy-image.",[1],"data-v-1d6a401a { height: 100%; width: 100%; -webkit-box-flex: 1; -webkit-flex: 1; -ms-flex: 1; flex: 1; display: -webkit-box; display: -webkit-flex; display: -ms-flexbox; display: flex; -webkit-box-orient: vertical; -webkit-box-direction: normal; -webkit-flex-direction: column; -ms-flex-direction: column; flex-direction: column; -webkit-box-align: center; -webkit-align-items: center; -ms-flex-align: center; align-items: center; position: relative; }\n.",[1],"lazy-image wx-image.",[1],"data-v-1d6a401a { width: 100%; }\n.",[1],"lazy-image wx-view.",[1],"data-v-1d6a401a { background-color: #eee; position: absolute; z-index: 2; top: 0; left: 0; height: 100%; width: 100%; -webkit-box-flex: 1; -webkit-flex: 1; -ms-flex: 1; flex: 1; display: -webkit-box; display: -webkit-flex; display: -ms-flexbox; display: flex; -webkit-box-orient: vertical; -webkit-box-direction: normal; -webkit-flex-direction: column; -ms-flex-direction: column; flex-direction: column; -webkit-box-align: center; -webkit-align-items: center; -ms-flex-align: center; align-items: center; -webkit-transition: opacity 0.4s linear; -o-transition: opacity 0.4s linear; transition: opacity 0.4s linear; }\n.",[1],"lazy-image wx-view wx-image.",[1],"data-v-1d6a401a { width: 100%; }\n.",[1],"lazy-image .",[1],"loaded.",[1],"data-v-1d6a401a { opacity: 0; }\n.",[1],"img-list{ width: ",[0,750],"; height:",[0,300],"; }\n",],undefined,{path:"./pages/lazy-load/lazy-load.wxss"})();
+document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/lazy-load/lazy-load.wxml') } }));
+        }
+        if(window.__uniAppViewReady__){
+          uniAppViewReadyCallback()
+        }else{
+          document.addEventListener('uniAppViewReady',uniAppViewReadyCallback)
+        }
+      })();
+      
